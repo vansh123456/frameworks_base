c8bb54c07c93e80af9227ff58c368025fcc80064
Adhitya Mohan <me@adhityamohan.in>
2020-12-08 02:34:24 +0530

    Revert "[SQUASH]base: Revert secondary home handle"
    This reverts commit dfffa0834c4ca8cdd250333e7ffdeaa53d50d0eb.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationHandle.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationHandle.java
@@ -35,11 +35,11 @@ import com.android.systemui.R;
 
 public class NavigationHandle extends View implements ButtonInterface {
 
-    private final Paint mPaint = new Paint();
+    protected final Paint mPaint = new Paint();
     private @ColorInt final int mLightColor;
     private @ColorInt final int mDarkColor;
-    private final int mRadius;
-    private final int mBottom;
+    protected final int mRadius;
+    protected final int mBottom;
     private boolean mIsDreaming = false;
     private boolean mIsKeyguard = false;
     private boolean mRequiresInvalidate;
