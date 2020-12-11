0c9535302171907921167e8776b188f503b18380
vansh123456 <vanshbhatnagar13@gmail.com>
2020-12-11 17:02:54 +0530

    Adapt to YAAP

--- core/java/com/android/internal/os/Zygote.java
+++ core/java/com/android/internal/os/Zygote.java
@@ -36,7 +36,7 @@ import android.system.ErrnoException;
 import android.system.Os;
 import android.util.Log;
 
-import com.android.internal.util.du.PixelPropsUtils;
+import com.android.internal.util.yaap.PixelPropsUtils;
 
 import dalvik.annotation.optimization.FastNative;
 import dalvik.system.ZygoteHooks;
--- core/java/com/android/internal/util/yaap/PixelPropsUtils.java
+++ core/java/com/android/internal/util/yaap/PixelPropsUtils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.android.internal.util.du;
+package com.android.internal.util.yaap;
 
 import android.os.Build;
 import android.util.Log;
